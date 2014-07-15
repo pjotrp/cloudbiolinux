359e0510d8af0e290b5341a384e22d110cc9b720
Pjotr Prins <pjotr.public01@thebird.nl>
2014-07-15 15:34:44 -0400

    Always have a short_name

--- cloudbio/flavor/__init__.py
+++ cloudbio/flavor/__init__.py
@@ -14,6 +14,7 @@ class Flavor:
     def __init__(self, env):
         self.name = "Base Flavor - no overrides" # should override this
         self.env = env
+        self.short_name = "flavor"
         self.check_distribution()
 
     def rewrite_config_items(self, name, items):
